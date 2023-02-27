24a77eb67e222c17a4e8dcb0b7b0d3a7f9c6927e
calebcheboi360 <calebcheboi0@gmail.com>
2023-02-27 01:40:53 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,8 @@
  * main - A programme that prints a line using the print f function
  * Return: 0 (success)
  */
-
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art, \n")
-		return (0);
+printf("with proper grammar, but the outcome is a piece of art, \n")
+return (0);
 }
